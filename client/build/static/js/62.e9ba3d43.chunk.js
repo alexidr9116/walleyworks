efda0aef629be4ee4bf926f6f5e4c62406d10f0f
alexidr9116 <alexdr9116@gmail.com>
2022-02-21 21:21:28 +0300

    walleywork

--- client/build/static/js/62.e9ba3d43.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpwalleyworks=this.webpackJsonpwalleyworks||[]).push([[62],{1237:function(s,a,e){"use strict";e.r(a);var t=e(2064);a.default=t.a}}]);
-//# sourceMappingURL=62.e9ba3d43.chunk.js.map
