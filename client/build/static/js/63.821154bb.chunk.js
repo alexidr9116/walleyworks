f86b992b8e86545fb86a189ea22c787e61c342b1
alexidr9116 <alexdr9116@gmail.com>
2022-02-22 13:51:11 +0300

    darkmode

---
+++ client/build/static/js/63.821154bb.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwalleyworks=this.webpackJsonpwalleyworks||[]).push([[63],{1237:function(s,a,e){"use strict";e.r(a);var t=e(2067);a.default=t.a}}]);
+//# sourceMappingURL=63.821154bb.chunk.js.map
